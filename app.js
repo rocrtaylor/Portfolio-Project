d755d94dd53c93e0f1e24c5b52ad253d5856d65a
Roc Taylor <rocrtaylor@gmail.com>
2022-01-27 16:00:48 -0600

    html css links done

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 
-
-
-
 $(() => {
 
+    // const $div1 = $('#firstDiv');
+    // const $div2 = $('#secondDiv');
+    // const $div3 = $('#thirdDiv');
+
+    // const division1 = () => {
+    //     $div1.css('background-color', 'red');
+    // }
+
+    // const division2 = () => {
+    //     $div2.css('background-color', 'blue').css('padding', '10px');
+    // }
+
+    // const division3 = () => {
+    //     $div3.css('background-color', 'green').css('padding', '20px')
+    // }
+
+    // $div1.on('click', division1);
+    // $div2.on('click', division2);
+    // $div3.on('click', division3);
 
 
-
-
-
-})
+});
